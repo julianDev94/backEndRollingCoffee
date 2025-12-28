4dbd2c9645da6f54fcb972a4467770b10ca735ce
JulianAballay <juliancabj1210@gmail.com>
2025-12-28 01:14:30 -0300

    configo los middleware cors, morgan y dotenv

--- index.js
+++ index.js
@@ -6,8 +6,9 @@
 // Uno para produccion y el otro para desarrollo
 // El de produccion es el start
 import express from 'express';
-console.log('Hola mundo v2 jsjaj')
-
+import cors from 'cors';
+import 'dotenv/config'; //permite procesar variables dee entorno. Es una manera rapida de configurar el middleware
+import morgan from 'morgan';
 //1- para ejecutar nuestro backend debemos configurar un puerto. express() es una instancia de express
 const app = express();
 // crear una variable con express
@@ -20,8 +21,23 @@ app.listen(app.get('port'),()=>{
 
 
 //2- configurar middleware del proyecto
+// esto nos ayuda a entender las variables de entorno, que reciba conexiones remotas, que entienda el formato JSON.
+// estos son funciones que se ejecutan en el backend generalmente antes de llegar a las rutas.
+// son funciones de js que le dan una habilidad especial al back.
+app.use(cors()); //permitir conexiones remotas
+app.use(morgan('dev')); //nos da info extra en la terminal. Esta es en dev
+//Estos middleware que se van a utilizar ya vienen en express
+//Este permite a mi backend interpretar el formato json y cuando reciba una solicitud por ej post para crear un producto pueda
+// extraer los datos del body y trabajar con esos datos
+app.use(express.json()); //permite interpretar datos en formato json
+app.use(express.urlencoded({extended: true})) //permite extraer los datos del body del request. {extended:true} es un objeto que extrae los datos
+// del body de la solicitud que llegue. Es un middleware de Express que permite leer datos enviados desde formularios HTML.
+//falta configurar el index.html
 
 //3- configurar las rutas
+
+
+
 //el comando node --watch index.js complica el archivo index.js y lo reinicia si detecta cambios. Este se lo utiliza en desarrollo
 // mejor utilizar para esto la liberia nodemoon -> mas estable, configurable
 // el -g del comando para instalar nodemon nos dice que se va instalar de forma global en todos los proyectos de express que queramos hacer
