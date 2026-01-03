5cb67c396e12347b4942263153fe45353c302f0e
JulianAballay <juliancabj1210@gmail.com>
2026-01-03 13:38:04 -0300

    agrego un metodo para obtener un producto por id

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,27 +1,38 @@
 import Producto from "../database/model/producto.js";
 
-
-  // console.log("Desde listar producto");
-  // res.send("Enviar lista productos...");
+// console.log("Desde listar producto");
+// res.send("Enviar lista productos...");
 export const listarProductos = async (req, res) => {
-
   //dentro del try no se puede responder 2 veces
   try {
-    //pedir a la bd la lista de la coleccion de documentos de productos 
+    //pedir a la bd la lista de la coleccion de documentos de productos
     const productos = await Producto.find({});
     res.status(200).json(productos);
   } catch (error) {
     console.log(error);
-    res.status(404).json({mensaje: "No se pudo encontrar la lista de productos"})
+    res
+      .status(404)
+      .json({ mensaje: "No se pudo encontrar la lista de productos" });
   }
-  
 };
 
-export const crearProducto = async (req, res)=>{
+// req.params me devuelve un objeto con los parametros que vengan en la url. en este caso como enviamos un id, me devuelve el objeto de este id
+// console.log(req.params.id);
+// req.params.id en este caso id porque es la palabra que me invente en la ruta para obtener el producto.
+export const obtenerUnProducto = async (req, res) => {
   try {
-    
+    const producto = await Producto.findById(req.params.id);
+    res.status(200).json(producto);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ mensaje: "El producto buscado no fue encontrado" });
+  }
+};
+
+export const crearProducto = async (req, res) => {
+  try {
     //1- verificar datos del body
-    console.log(req.body)
+    console.log(req.body);
     //2- validar los datos
 
     //3- pedir a la BD crear el producto
@@ -29,13 +40,11 @@ export const crearProducto = async (req, res)=>{
     await productoNuevo.save();
 
     //4- enviar status 201
-    res.status(201).json({mensaje: "El producto fue creado correctamente"});
-    
+    res.status(201).json({ mensaje: "El producto fue creado correctamente" });
   } catch (error) {
-   console.log(error); 
-   res.status(400).json({
-    mensaje: "No se pudo procesar la solicitud de crear Producto"
-   });
-
+    console.log(error);
+    res.status(400).json({
+      mensaje: "No se pudo procesar la solicitud de crear Producto",
+    });
   }
-}
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { crearProducto, listarProductos } from "../controllers/productos.controllers.js";
+import { obtenerUnProducto } from "../controllers/productos.controllers.js";
 // De express import un enrutador para definir las rutas y la instancio
 const router = Router();
 
 router.route('/productos').get(listarProductos).post(crearProducto)
-
+router.route('/producto/:id').get(obtenerUnProducto);
 export default router;
