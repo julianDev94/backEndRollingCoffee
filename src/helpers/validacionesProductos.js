c67f85431b6b8aa2fab1b4997b23f4cb0015d3a7
JulianAballay <juliancabj1210@gmail.com>
2026-01-06 20:49:27 -0300

    agrego las ultimas validaciones de express validator

--- src/helpers/validacionesProductos.js
+++ src/helpers/validacionesProductos.js
@@ -19,9 +19,31 @@ export const validarProducto = [
         throw new Error("El precio debe estar entre $50 y $10000");
       }
     }),
-    (req, res, next)=>{
-        resultadoValidacion(req, res,next)
-    }
+  check("urlImagen")
+    .notEmpty()
+    .withMessage("La url de la imagen es obligatoria")
+    .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+    .withMessage("La imagen debe tener un formato de URL valida"),
+  check("categoria")
+    .notEmpty()
+    .withMessage("La categoria es obligatoria")
+    .isIn(["Infusiones", "Batidos", "Dulce"])
+    .withMessage(
+      "La categoria debe ser una de las siguientes: Infusiones, Batidos, Dulce "
+    ),
+  check("descripcionBreve")
+    .notEmpty()
+    .withMessage("La descripcion breve es obligatoria")
+    .isLength({ min: 5, max: 50 })
+    .withMessage("La cantidad de caracteres debe estar entre 2 y 40"),
+  check("descripcionAmplia")
+    .notEmpty()
+    .withMessage("La descripcion amplia es obligatoria")
+    .isLength({ min: 5, max: 200 })
+    .withMessage("La cantidad de caracteres debe estar entre 2 y 40"),
+  (req, res, next) => {
+    resultadoValidacion(req, res, next);
+  },
 ];
 
 export default validarProducto;
